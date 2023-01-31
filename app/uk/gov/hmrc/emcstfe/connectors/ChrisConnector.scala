b632dc267763adc8b69ecab7278e54c267720b0d
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2023-01-31 18:43:02 +0000

    [ETFE-444] Remove HelloWorld related code

--- app/uk/gov/hmrc/emcstfe/connectors/ChrisConnector.scala
+++ app/uk/gov/hmrc/emcstfe/connectors/ChrisConnector.scala
@@ -17,50 +17,24 @@
 package uk.gov.hmrc.emcstfe.connectors
 
 import com.lucidchart.open.xtract.XmlReader
-import play.api.http.Status._
 import uk.gov.hmrc.emcstfe.config.AppConfig
 import uk.gov.hmrc.emcstfe.connectors.httpParsers.ChrisXMLHttpParser
 import uk.gov.hmrc.emcstfe.models.request.ChrisRequest
-import uk.gov.hmrc.emcstfe.models.response.ErrorResponse._
-import uk.gov.hmrc.emcstfe.models.response.{ErrorResponse, HelloWorldResponse}
-import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpReads, HttpResponse}
+import uk.gov.hmrc.emcstfe.models.response.ErrorResponse
+import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 
 @Singleton
 class ChrisConnector @Inject()(val http: HttpClient,
-                               val config: AppConfig,
+                               override val appConfig: AppConfig,
                                chrisHttpParser: ChrisXMLHttpParser
                               ) extends BaseConnector {
 
-  override def appConfig: AppConfig = config
-
-  def hello()(implicit headerCarrier: HeaderCarrier, ec: ExecutionContext): Future[Either[ErrorResponse, HelloWorldResponse]] = {
-    val url: String = s"${config.chrisUrl}/hello-world"
-
-    //TODO: Temporary, this entire method will be removed but this is needed for now to prevent thrown exceptions from HttpClient library
-    implicit val rdsHttpResponse: HttpReads[HttpResponse] = (_: String, _: String, response: HttpResponse) => response
-
-    http.GET[HttpResponse](url).map {
-      response =>
-        response.status match {
-          case OK => response.validateJson[HelloWorldResponse] match {
-            case Some(valid) => Right(valid)
-            case None =>
-              logger.warn(s"[ChrisConnector] Bad JSON response from emcs-tfe-chris-stub")
-              Left(JsonValidationError)
-          }
-          case status =>
-            logger.warn(s"[ChrisConnector] Unexpected status from emcs-tfe-chris-stub: $status")
-            Left(UnexpectedDownstreamResponseError)
-        }
-    }
-  }
-
   def postChrisSOAPRequest[A](request: ChrisRequest)
                              (implicit headerCarrier: HeaderCarrier, ec: ExecutionContext, xmlRds: XmlReader[A]): Future[Either[ErrorResponse, A]] = {
-    val url: String = s"${config.chrisUrl}/ChRISOSB/EMCS/EMCSApplicationService/2"
+    val url: String = s"${appConfig.chrisUrl}/ChRISOSB/EMCS/EMCSApplicationService/2"
     postString(http, url, request.requestBody, request.action)(ec, headerCarrier, chrisHttpParser.rawXMLHttpReads)
   }
 }
--- test/uk/gov/hmrc/emcstfe/connectors/ChrisConnectorSpec.scala
+++ test/uk/gov/hmrc/emcstfe/connectors/ChrisConnectorSpec.scala
@@ -17,16 +17,15 @@
 package uk.gov.hmrc.emcstfe.connectors
 
 import play.api.http.{HeaderNames, MimeTypes, Status}
-import play.api.libs.json.{Json, OFormat}
 import uk.gov.hmrc.emcstfe.connectors.httpParsers.ChrisXMLHttpParser
 import uk.gov.hmrc.emcstfe.fixtures.GetMovementFixture
 import uk.gov.hmrc.emcstfe.mocks.config.MockAppConfig
 import uk.gov.hmrc.emcstfe.mocks.connectors.MockHttpClient
 import uk.gov.hmrc.emcstfe.models.request.GetMovementRequest
-import uk.gov.hmrc.emcstfe.models.response.ErrorResponse.{JsonValidationError, UnexpectedDownstreamResponseError, XmlValidationError}
-import uk.gov.hmrc.emcstfe.models.response.{GetMovementResponse, HelloWorldResponse}
+import uk.gov.hmrc.emcstfe.models.response.ErrorResponse.{UnexpectedDownstreamResponseError, XmlValidationError}
+import uk.gov.hmrc.emcstfe.models.response.GetMovementResponse
 import uk.gov.hmrc.emcstfe.support.UnitSpec
-import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
+import uk.gov.hmrc.http.HeaderCarrier
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -41,41 +40,6 @@ class ChrisConnectorSpec extends UnitSpec with Status with MimeTypes with Header
     val baseUrl: String = "http://test-BaseUrl"
     MockedAppConfig.chrisUrl.returns(baseUrl)
     MockedAppConfig.chrisHeaders.returns(Seq()).anyNumberOfTimes()
-  }
-
-  "hello" should {
-    "return a Right" when {
-      "downstream call is successful" in new Test {
-        val response: HttpResponse = HttpResponse(status = Status.OK, json = Json.toJson(HelloWorldResponse("test message")), headers = Map.empty)
-
-        MockHttpClient.get(s"$baseUrl/hello-world").returns(Future.successful(response))
-
-        await(connector.hello()) shouldBe Right(HelloWorldResponse("test message"))
-      }
-    }
-    "return a Left" when {
-      "downstream call is successful but doesn't match expected JSON" in new Test {
-
-        case class TestModel(field: String)
-
-        object TestModel {
-          implicit val format: OFormat[TestModel] = Json.format
-        }
-
-        val response: HttpResponse = HttpResponse(status = Status.OK, json = Json.toJson(TestModel("test message")), headers = Map.empty)
-
-        MockHttpClient.get(s"$baseUrl/hello-world").returns(Future.successful(response))
-
-        await(connector.hello()) shouldBe Left(JsonValidationError)
-      }
-      "downstream call is unsuccessful" in new Test {
-        val response: HttpResponse = HttpResponse(status = Status.INTERNAL_SERVER_ERROR, json = Json.toJson(HelloWorldResponse("test message")), headers = Map.empty)
-
-        MockHttpClient.get(s"$baseUrl/hello-world").returns(Future.successful(response))
-
-        await(connector.hello()) shouldBe Left(UnexpectedDownstreamResponseError)
-      }
-    }
   }
 
   "getMovement" should {
@@ -97,7 +61,6 @@ class ChrisConnectorSpec extends UnitSpec with Status with MimeTypes with Header
       }
     }
     "return a Left" when {
-      //TODO test BaseConnector's chrisReads function
       "downstream call is successful but can't convert the response to XML" in new Test {
 
         val response = Left(XmlValidationError)
