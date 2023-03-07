e21a098dfd58ce9227483269bcdc2c7c200ad60a
james-work-account <31282758+james-work-account@users.noreply.github.com>
2023-03-07 14:08:18 +0000

    [ETFE-691] make service work with Java 11

--- app/uk/gov/hmrc/emcstfe/utils/HMRCMarkUtil.scala
+++ app/uk/gov/hmrc/emcstfe/utils/HMRCMarkUtil.scala
@@ -21,6 +21,7 @@ import com.sun.org.apache.xml.internal.security.c14n.Canonicalizer
 import uk.gov.hmrc.emcstfe.models.response.ErrorResponse
 import uk.gov.hmrc.emcstfe.models.response.ErrorResponse.MarkCreationError
 
+import java.io.ByteArrayOutputStream
 import java.security.MessageDigest
 import java.util.Base64
 import javax.inject.{Inject, Singleton}
@@ -30,23 +31,23 @@ import scala.xml.Elem
 @Singleton
 class HMRCMarkUtil @Inject()() extends Logging {
 
-  def createHmrcMark(xml: Elem): Either[ErrorResponse, String] = {
-    Try {
+  def createHmrcMark(xml: Elem): Either[ErrorResponse, String] = Try {
 
-      Init.init()
+    Init.init()
 
-      val c14n: Canonicalizer = Canonicalizer.getInstance(Canonicalizer.ALGO_ID_C14N_OMIT_COMMENTS)
+    val c14n: Canonicalizer = Canonicalizer.getInstance(Canonicalizer.ALGO_ID_C14N_OMIT_COMMENTS)
 
-      val trimmedXml = scala.xml.Utility.trim((xml \ "Body").head)
-      val xmlBytes = trimmedXml.toString().getBytes()
-      val canonXmlBytes: Array[Byte] = c14n.canonicalize(xmlBytes)
+    val trimmedXml = scala.xml.Utility.trim((xml \ "Body").head)
+    val xmlBytes = trimmedXml.toString().getBytes()
+    val canonStream: ByteArrayOutputStream = new ByteArrayOutputStream()
+    c14n.canonicalize(xmlBytes, canonStream, true)
+    val canonXmlBytes: Array[Byte] = canonStream.toByteArray
 
-      Base64.getEncoder.encodeToString(MessageDigest.getInstance("SHA-1").digest(canonXmlBytes))
-    } match {
-      case Failure(exception) =>
-        logger.warn(exception.getMessage)
-        Left(MarkCreationError)
-      case Success(value) => Right(value)
-    }
+    Base64.getEncoder.encodeToString(MessageDigest.getInstance("SHA-1").digest(canonXmlBytes))
+  } match {
+    case Failure(exception) =>
+      logger.warn(exception.getMessage)
+      Left(MarkCreationError)
+    case Success(value) => Right(value)
   }
 }
