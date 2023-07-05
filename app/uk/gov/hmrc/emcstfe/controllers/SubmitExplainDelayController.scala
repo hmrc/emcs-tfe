f313ae70dc7ce0e63b7158f18bc9099c2630e2b8
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2023-07-05 16:36:56 +0100

    [ETFE-1423] Added Controller to parse submission and call service

---
+++ app/uk/gov/hmrc/emcstfe/controllers/SubmitExplainDelayController.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.emcstfe.controllers
+
+import play.api.libs.json.{JsValue, Json}
+import play.api.mvc.{Action, ControllerComponents}
+import uk.gov.hmrc.emcstfe.controllers.actions.{AuthAction, AuthActionHelper, UserAllowListAction}
+import uk.gov.hmrc.emcstfe.models.explainDelay.SubmitExplainDelayModel
+import uk.gov.hmrc.emcstfe.services.SubmitExplainDelayService
+import uk.gov.hmrc.emcstfe.utils.Logging
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
+
+@Singleton()
+class SubmitExplainDelayController @Inject()(cc: ControllerComponents,
+                                             service: SubmitExplainDelayService,
+                                             override val auth: AuthAction,
+                                             override val userAllowList: UserAllowListAction
+                                            )(implicit ec: ExecutionContext) extends BackendController(cc) with AuthActionHelper with Logging {
+
+  def submit(ern: String, arc: String): Action[JsValue] = authorisedUserSubmissionRequest(ern) { implicit request =>
+    withJsonBody[SubmitExplainDelayModel] {
+      service.submit(_).map {
+        case Left(value) => InternalServerError(Json.toJson(value))
+        case Right(value) => Ok(Json.toJson(value))
+      }
+    }
+  }
+}
