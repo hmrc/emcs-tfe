88f6c42a0e16b881ae39d5e0c7a92bf04da2d413
Richard Melvin <40235254+r-melvin@users.noreply.github.com>
2023-07-28 09:46:04 +0100

    ETFE-1509 - Fix - Updated XML construction

--- test/uk/gov/hmrc/emcstfe/models/request/SubmitCancellationOfMovementRequestSpec.scala
+++ test/uk/gov/hmrc/emcstfe/models/request/SubmitCancellationOfMovementRequestSpec.scala
@@ -42,7 +42,7 @@ class SubmitCancellationOfMovementRequestSpec extends UnitSpec with SubmitCancel
               <ns:VendorID>1259</ns:VendorID>
               <ns:VendorProduct Version="2.0">HMRC Portal</ns:VendorProduct>
               <ns:ServiceID>1138</ns:ServiceID>
-              <ns:ServiceMessageType>HMRC-EMCS-IE819-DIRECT</ns:ServiceMessageType>
+              <ns:ServiceMessageType>HMRC-EMCS-IE810-DIRECT</ns:ServiceMessageType>
             </ns:Info>
             <MetaData xmlns="http://www.hmrc.gov.uk/ChRIS/SOAP/MetaData/1">
               <CredentialID>{testCredId}</CredentialID>
@@ -50,27 +50,19 @@ class SubmitCancellationOfMovementRequestSpec extends UnitSpec with SubmitCancel
             </MetaData>
           </soapenv:Header>
           <soapenv:Body>
-            <urn:IE819 xmlns:urn="urn:publicid:-:EC:DGTAXUD:EMCS:PHASE4:IE819:V3.01" xmlns:urn1="urn:publicid:-:EC:DGTAXUD:EMCS:PHASE4:TMS:V3.01">
+            <urn:IE810 xmlns:urn="urn:publicid:-:EC:DGTAXUD:EMCS:PHASE4:IE810:V3.01" xmlns:urn1="urn:publicid:-:EC:DGTAXUD:EMCS:PHASE4:TMS:V3.01">
               <urn:Header>
                 <urn1:MessageSender>{request.messageSender}</urn1:MessageSender>
                 <urn1:MessageRecipient>{request.messageRecipient}</urn1:MessageRecipient>
-                <urn1:DateOfPreparation>
-                  {request.preparedDate.toString}
-                </urn1:DateOfPreparation>
-                <urn1:TimeOfPreparation>
-                  {request.preparedTime.toString}
-                </urn1:TimeOfPreparation>
-                <urn1:MessageIdentifier>
-                  {request.messageUUID}
-                </urn1:MessageIdentifier>
-                <urn1:CorrelationIdentifier>
-                  {request.correlationUUID}
-                </urn1:CorrelationIdentifier>
+                <urn1:DateOfPreparation>{request.preparedDate.toString}</urn1:DateOfPreparation>
+                <urn1:TimeOfPreparation>{request.preparedTime.toString}</urn1:TimeOfPreparation>
+                <urn1:MessageIdentifier>{request.messageUUID}</urn1:MessageIdentifier>
+                <urn1:CorrelationIdentifier>{request.correlationUUID}</urn1:CorrelationIdentifier>
               </urn:Header>
               <urn:Body>
                 {maxSubmitCancellationOfMovementModelXml}
               </urn:Body>
-            </urn:IE819>
+            </urn:IE810>
           </soapenv:Body>
         </soapenv:Envelope>
 
