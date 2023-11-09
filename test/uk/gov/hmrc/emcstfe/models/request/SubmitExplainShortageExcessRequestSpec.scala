c49217b3e33e84ecbf69e57bb0469c7e505135e2
Richard Melvin <40235254+r-melvin@users.noreply.github.com>
2023-11-09 15:07:02 +0000

    Added tests

--- test/uk/gov/hmrc/emcstfe/models/request/SubmitExplainShortageExcessRequestSpec.scala
+++ test/uk/gov/hmrc/emcstfe/models/request/SubmitExplainShortageExcessRequestSpec.scala
@@ -16,12 +16,14 @@
 
 package uk.gov.hmrc.emcstfe.models.request
 
+import play.api.libs.json.Json
 import uk.gov.hmrc.emcstfe.fixtures.{SubmitExplainShortageExcessFixtures, TraderModelFixtures}
 import uk.gov.hmrc.emcstfe.models.common.SubmitterType.Consignor
 import uk.gov.hmrc.emcstfe.models.common.{ConsigneeTrader, SubmitterType, TraderModel}
 import uk.gov.hmrc.emcstfe.models.explainShortageExcess.AttributesModel
 import uk.gov.hmrc.emcstfe.support.TestBaseSpec
 
+import java.util.Base64
 import scala.xml.Utility.trim
 import scala.xml.XML
 
@@ -179,8 +181,53 @@ class SubmitExplainShortageExcessRequestSpec extends TestBaseSpec with SubmitExp
           val request = SubmitExplainShortageExcessRequest(submitExplainShortageExcessModelMax(Consignor))
           request.exciseRegistrationNumber shouldBe testErn
         }
+      }
 
+      ".eisXMLBody" should {
+        "generate the correct XML body" in {
+          val request = SubmitExplainShortageExcessRequest(submitExplainShortageExcessModelMax(Consignor))
 
+          val expectedRequest =
+            <urn:IE871 xmlns:urn1="urn:publicid:-:EC:DGTAXUD:EMCS:PHASE4:TMS:V3.01" xmlns:urn="urn:publicid:-:EC:DGTAXUD:EMCS:PHASE4:IE871:V3.01">
+              <urn:Header>
+                <urn1:MessageSender>
+                  {request.messageSender}
+                </urn1:MessageSender>
+                <urn1:MessageRecipient>
+                  {request.messageRecipient}
+                </urn1:MessageRecipient>
+                <urn1:DateOfPreparation>
+                  {request.preparedDate.toString}
+                </urn1:DateOfPreparation>
+                <urn1:TimeOfPreparation>
+                  {request.preparedTime.toString}
+                </urn1:TimeOfPreparation>
+                <urn1:MessageIdentifier>
+                  {request.messageUUID}
+                </urn1:MessageIdentifier>
+                <urn1:CorrelationIdentifier>
+                  {request.correlationUUID}
+                </urn1:CorrelationIdentifier>
+              </urn:Header>
+              <urn:Body>
+                {submitExplainShortageExcessXmlMax(Consignor)}
+              </urn:Body>
+            </urn:IE871>
+          trim(XML.loadString(request.eisXMLBody())).toString shouldBe trim(expectedRequest).toString
+        }
+      }
+
+      ".toJson" should {
+        "create the correct JSON body" in {
+          val request = SubmitExplainShortageExcessRequest(submitExplainShortageExcessModelMax(Consignor))
+
+          val expectedResult = Json.obj(
+            "user" -> testErn,
+            "messageType" -> "IE871",
+            "message" -> Base64.getEncoder.encodeToString(XML.loadString(request.eisXMLBody()).toString().getBytes)
+          )
+          request.toJson shouldBe expectedResult
+        }
       }
     }
   }
