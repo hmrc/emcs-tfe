ae3d205fab221574d6bb12f3380bb756dd8c11ec
james-work-account <31282758+james-work-account@users.noreply.github.com>
2024-09-17 14:38:55 +0100

    [ETFE-4527] controller

---
+++ it/uk/gov/hmrc/emcstfe/controllers/TraderKnownFactsIntegrationSpec.scala
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.emcstfe.controllers
+
+import com.github.tomakehurst.wiremock.stubbing.StubMapping
+import play.api.http.Status
+import play.api.http.Status.FORBIDDEN
+import play.api.libs.json.Json
+import play.api.libs.ws.{WSRequest, WSResponse}
+import uk.gov.hmrc.emcstfe.fixtures.TraderKnownFactsFixtures
+import uk.gov.hmrc.emcstfe.models.response.ErrorResponse._
+import uk.gov.hmrc.emcstfe.stubs.{AuthStub, DownstreamStub}
+import uk.gov.hmrc.emcstfe.support.IntegrationBaseSpec
+
+class TraderKnownFactsIntegrationSpec extends IntegrationBaseSpec with TraderKnownFactsFixtures {
+
+  private trait Test {
+    def setupStubs(): StubMapping
+
+    def uri: String = s"/trader-known-facts"
+
+    def emcsTfeReferenceDataUrl: String = s"/emcs-tfe-reference-data/oracle/trader-known-facts"
+
+    def downstreamQueryParams: Map[String, String] = Map(
+      "exciseRegistrationId" -> testErn
+    )
+
+    def request(): WSRequest = {
+      setupStubs()
+      buildRequest(uri).withQueryStringParameters("exciseRegistrationId" -> testErn)
+    }
+
+  }
+
+  "Calling the get trader known facts endpoint" when {
+
+    "user is unauthorised" must {
+      s"return FORBIDDEN ($FORBIDDEN)" in new Test {
+        override def setupStubs(): StubMapping = {
+          AuthStub.unauthorised()
+        }
+
+        val response: WSResponse = await(request().get())
+        response.status shouldBe FORBIDDEN
+      }
+    }
+
+    "user is authorised" when {
+
+      "return forbidden" when {
+        "the ERN requested does not match the ERN of the credential" in new Test {
+          override def setupStubs(): StubMapping = {
+            AuthStub.authorised("WrongERN")
+          }
+
+          val response: WSResponse = await(request().get())
+          response.status shouldBe Status.FORBIDDEN
+        }
+      }
+
+      "return a success" when {
+        "all downstream calls are successful" in new Test {
+          override def setupStubs(): StubMapping = {
+            AuthStub.authorised()
+            DownstreamStub.onSuccess(DownstreamStub.GET, emcsTfeReferenceDataUrl, downstreamQueryParams, Status.OK, Json.parse(traderKnownFactsCandEJson))
+          }
+
+          val response: WSResponse = await(request().get())
+          response.status shouldBe Status.OK
+          response.header("Content-Type") shouldBe Some("application/json")
+          response.json shouldBe Json.parse(testTraderKnownFactsJson)
+        }
+        "return an error" when {
+          "downstream call returns an unexpected HTTP response" in new Test {
+            override def setupStubs(): StubMapping = {
+              AuthStub.authorised()
+              DownstreamStub.onSuccess(DownstreamStub.GET, emcsTfeReferenceDataUrl, downstreamQueryParams, Status.NO_CONTENT, Json.obj())
+            }
+
+            val response: WSResponse = await(request().get())
+            response.status shouldBe Status.INTERNAL_SERVER_ERROR
+            response.header("Content-Type") shouldBe Some("application/json")
+            response.json shouldBe Json.toJson(UnexpectedDownstreamResponseError)
+          }
+        }
+      }
+    }
+  }
+
+}
---
+++ test/uk/gov/hmrc/emcstfe/mocks/services/MockTraderKnownFactsService.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.emcstfe.mocks.services
+
+import org.scalamock.handlers.CallHandler3
+import org.scalamock.scalatest.MockFactory
+import uk.gov.hmrc.emcstfe.models.response.{ErrorResponse, TraderKnownFacts}
+import uk.gov.hmrc.emcstfe.services.TraderKnownFactsService
+import uk.gov.hmrc.http.HeaderCarrier
+
+import scala.concurrent.{ExecutionContext, Future}
+
+trait MockTraderKnownFactsService extends MockFactory {
+  lazy val mockService: TraderKnownFactsService = mock[TraderKnownFactsService]
+
+  object MockService {
+
+    def getTraderKnownFacts(ern: String): CallHandler3[String, HeaderCarrier, ExecutionContext, Future[Either[ErrorResponse, TraderKnownFacts]]] = {
+      (mockService
+        .getTraderKnownFacts(_: String)(_: HeaderCarrier, _: ExecutionContext))
+        .expects(ern, *, *)
+    }
+
+  }
+
+}
